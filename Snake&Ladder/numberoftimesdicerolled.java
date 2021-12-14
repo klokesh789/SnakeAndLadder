a45a772717b1fbe778b52bdfa74eeb07892e1535
lokesh <kottelokesh789@gmail.com>
2021-12-14 22:29:13 +0530

    ADDED: RolledDicePosition

---
+++ Snake&Ladder/numberoftimesdicerolled.java
@@ -0,0 +1,35 @@
+public class positionDiceRoll {
+
+        public static final int noPlayOption = 0;
+        public static final int ladderOption = 1;
+        public static final int snakeOption = 2;
+        public static int playerOne = 1;
+        public static void main(String[] args){
+        double position = 0;
+	double diceRollCount = 0;
+        while (position < 100){
+               double diceRoll = (Math.floor(Math.random() * 10) % 6) + 1;
+               System.out.println("Dice rolled value: " + diceRoll);
+               int option = (int) Math.floor(Math.random() * 10) % 3;
+                switch (option) {
+                        case noPlayOption:
+				System.out.println("Position: " + position);
+                        	break;
+                        case ladderOption:
+                                if(position + diceRoll <= 100){
+                                        position = position + diceRoll;
+                                }
+                                break;
+                        case snakeOption:
+                        position = position - diceRoll;
+                        if (position < 0)
+                        {
+                            position = 0;
+                        }
+			System.out.println("Position: " + position);
+                        break;
+                }
+        }
+         System.out.println("Player Wins with Position :" + position);
+        }
+}
